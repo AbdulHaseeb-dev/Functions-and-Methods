f9ff16c44b84fe25919117da118346ee5d2a804b
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-03-18 16:09:08 +0500

    practising functions

---
+++ myName.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class myName {
+    public static void printMyName(String name){
+        System.out.println(name);   
+        return;         
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name");
+        String name = sc.nextLine();
+        printMyName(name);
+        sc.close();
+    }
+}
