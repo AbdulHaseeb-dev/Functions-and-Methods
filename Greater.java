f4f238d543c7f405abb896b4f8d144dd66667a3e
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-03-20 10:43:40 +0500

    practising functions

---
+++ Greater.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Greater {
+    public static void greaterNumber(int a, int b){
+        if(a>b){
+        System.out.println(a);
+    }else {
+        System.out.println(b);
+    }
+
+    }
+    public static void main (String[] args){
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a");
+        int a = sc.nextInt();
+        System.out.println("Enter b");
+        int b = sc.nextInt();
+        greaterNumber(a, b);
+        sc.close();
+    }
+}
